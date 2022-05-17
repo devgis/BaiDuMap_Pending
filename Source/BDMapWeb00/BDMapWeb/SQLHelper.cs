b6a8e0b7b5674a56c533b76aaad84078d9a700b1
devgis <devgis@163.com>
2022-05-17 20:23:56 +0800

    Public
    Public

---
+++ Source/BDMapWeb00/BDMapWeb/SQLHelper.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BDMapWeb
+{
+    public class SQLHelper
+    {
+        private static SqlConnection SqlConn;
+        static SQLHelper()
+        {
+            if (SqlConn == null)
+            {
+                SqlConn = new SqlConnection();
+                SqlConn.ConnectionString = System.Configuration.ConfigurationManager.AppSettings["SqlStr"].ToString();
+            }
+        }
+        public static DataTable GetDataTable(string sql, SqlParameter[] parameters)
+        {
+
+            try
+            {
+                DataTable dt = new DataTable();
+                SqlCommand sqlCmd = new SqlCommand();
+                sqlCmd.CommandText = sql;
+                if (parameters != null)
+                {
+                    sqlCmd.Parameters.AddRange(parameters);
+                }
+                sqlCmd.Connection = SqlConn;
+
+                SqlDataAdapter da = new SqlDataAdapter(sqlCmd);
+                da.Fill(dt);
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                SqlConn.Close();
+            }
+        }
+
+        public static DataTable GetDataTable(string sql)
+        {
+
+            try
+            {
+                return GetDataTable(sql, null);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                SqlConn.Close();
+            }
+        }
+
+        public static int ExecSql(string sql)
+        {
+            try
+            {
+                SqlCommand sqlCmd = new SqlCommand();
+                sqlCmd.CommandText = sql;
+                sqlCmd.Connection = SqlConn;
+                int i = sqlCmd.ExecuteNonQuery();
+                return i;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                SqlConn.Close();
+            }
+        }
+
+        public static int ExecSql(string sql, SqlParameter[] parameters)
+        {
+            try
+            {
+                SqlConn.Open();
+                SqlCommand sqlCmd = new SqlCommand();
+                sqlCmd.CommandText = sql;
+                if (parameters != null)
+                {
+                    sqlCmd.Parameters.AddRange(parameters);
+                }
+                sqlCmd.Connection = SqlConn;
+                int i = sqlCmd.ExecuteNonQuery();
+                return i;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                SqlConn.Close();
+            }
+        }
+    }
+}
---
+++ Source/BDMapWeb00/BDMapWeb/data.ashx
@@ -0,0 +1,21 @@
+﻿<%@ WebHandler Language="C#" Class="data" %>
+
+using System;
+using System.Web;
+using Newtonsoft.Json;
+
+public class data : IHttpHandler {
+    
+    public void ProcessRequest (HttpContext context) {
+        context.Response.ContentType = "text/plain";
+        System.Data.DataTable dt = BDMapWeb.SQLHelper.GetDataTable("select 箱体编号 as boxid,容积 as rj,经度 as lon,纬度 as lat from Box_GPS");
+        context.Response.Write(JsonConvert.SerializeObject(dt));
+    }
+ 
+    public bool IsReusable {
+        get {
+            return false;
+        }
+    }
+
+}
